430e7ddd9a0797341a26d63f2f0cb66c3efb3ca7
Luac <lo.accard@gmail.com>
2023-10-11 14:39:47 +0200

    new start

--- StatusUnknown/Assets/Scripts/LevelStreaming/LevelStreamVolume.cs
+++ StatusUnknown/Assets/Scripts/LevelStreaming/LevelStreamVolume.cs
@@ -1,22 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-
-namespace LevelStreaming
+public class LevelStreamVolume : MonoBehaviour
 {
-
-    using UnityEngine;
-    using LevelStreaming.Data;
-    [System.Serializable]
-    public class LevelStreamVolume
+    // Start is called before the first frame update
+    void Start()
     {
-        [field:SerializeField] public LevelStreamVolumeData Data { get; private set; }
-        public bool IsLoaded = false;
-        public bool FirstLoad = false;
-        public GameObject LoadRootObject;
+        
+    }
 
-        public LevelStreamVolume(LevelStreamVolumeData data,GameObject loadRootObject)
-        {
-            Data = data;
-            LoadRootObject = loadRootObject;
-        }
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
