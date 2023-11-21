856c018e458ea69bdbc9e39ed10b3763a6acb580
Félix COURTEMANCHE <f.courtemanche@rubika-edu.com>
2023-11-21 16:36:43 +0100

    Door proto start

---
+++ StatusUnknown/Assets/Scripts/GD_Draft/AutoDoor.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDoor : MonoBehaviour
+{
+    bool isOpen;
+    Animator animator;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+
+    void ChangeDoorState(bool open = true)
+    {
+        animator.SetTrigger();
+    }
+}
