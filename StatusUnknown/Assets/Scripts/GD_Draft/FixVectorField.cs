faeea04dcd9687a1f4846f394f5934a1c925f4f7
Lazuli <lazuli.combes@gmail.com>
2023-11-28 16:33:59 +0100

    Set Up VectorField + Fix

---
+++ StatusUnknown/Assets/Scripts/GD_Draft/FixVectorField.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VectorField;
+
+public class FixVectorField : MonoBehaviour
+{
+    public VectorFieldVolume vectorField;
+
+    private void Start()
+    {
+        vectorField.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            
+        }
+        vectorField.enabled = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        vectorField.enabled = false;
+    }
+}
